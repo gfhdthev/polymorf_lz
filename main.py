76462d4452825e2da506777ceee219848ca951e3
gfhdthev <akkglow@tutamail.com>
2025-04-24 13:22:53 +0300

    ///

---
+++ main.py
@@ -0,0 +1,13 @@
+#Gfhdthev
+#Лабораторная работа 4, основной файл
+
+
+from var11 import Bank #импортируем класс
+
+def main():
+    first = Bank()#создаем объект класса
+    +first
+    first.oper()
+
+if __name__ == '__main__': #проверка на прямой запуск
+    main()
---
+++ var11.py
@@ -0,0 +1,31 @@
+#Gfhdthev
+#Основной файл для лабораторной работы номер 4
+
+
+#импорт библиотек
+import pandas as pd
+
+
+#создаем основной класс
+class Bank:
+    def __init__(self): #инициализация
+        self.file = pd.read_csv('var11.csv')
+
+    def __pos__(self): #доопределения унарного +
+        self.df = self.file.drop_duplicates() #удаляем дубликаты строк
+        first_count = self.file.shape[0] #считаем кол-ыо дубликатов
+        second_count = self.df.shape[0]
+        dropped = first_count - second_count
+        print(f'В файле удалено {dropped} дубликатов')
+
+    def oper(self): #разделение датафрейма на 2 разных
+
+        self.df1 = self.df[self.df['Время оплаты'] >= '09:00:00']    # строки, удовлетворяющие условию
+        self.df1 = self.df[self.df['Время оплаты'] <= '23:00:00']    # строки, удовлетворяющие условию
+        self.df1.to_csv('first.csv')
+        self.df2 = self.df[self.df['Время оплаты'] <= '09:00:00']    # строки, не удовлетворяющие условию
+        self.df2 = self.df[self.df['Время оплаты'] >= '23:00:00']    # строки, не удовлетворяющие условию
+        self.df2.to_csv('second.csv')
+
+    def __del__(self):
+        print('Датафрейм удален')
